decc41c68ba69083bbf2d84c4f415646c6bb0867
tuckermcelroy <neidev1@yahoo.com>
2017-12-06 12:13:44 -0500

    added github to directory

--- tests/load_everything.R
+++ tests/load_everything.R
@@ -1,10 +1,10 @@
-install.packages("~/sigex/", repos = NULL, type = "source")
+install.packages("~/GitHub/sigex/", repos = NULL, type = "source")
 library(sigex)
 ls("package:sigex")
 lsf.str("package:sigex")
 
 # Check which file has an error
-for (f in list.files("~/sigex/R/", full.names=TRUE)){
+for (f in list.files("~/GitHub/sigex/R/", full.names=TRUE)){
   print(f)
   parse(f)
 }
