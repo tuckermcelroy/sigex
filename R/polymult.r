9fc8ab3c363afe51bd89a94179a8476eb3cce125
U-BOC\livse301 <livse301@90HQPLSU1253447.boc.ad.census.gov>
2018-09-20 06:45:49 -0400

    document polymult

--- R/polymult.r
+++ R/polymult.r
@@ -1,9 +1,18 @@
+#' multiply two power series coeficient vectors
+#'
+#' @param a power series coefficient vector
+#' @param b power series coefficient vector
+#'
+#' @return cofficient vector of product
+#' @export
+#'
+
 polymult <- function(a,b) {
-bb <- c(b,rep(0,length(a)-1))
-B <- toeplitz(bb)
-B[lower.tri(B)] <- 0
-aa <- rev(c(a,rep(0,length(b)-1)))
-prod <- B %*% matrix(aa,length(aa),1)
-return(rev(prod[,1]))
+  bb <- c(b,rep(0,length(a)-1))
+  B <- toeplitz(bb)
+  B[lower.tri(B)] <- 0
+  aa <- rev(c(a,rep(0,length(b)-1)))
+  prod <- B %*% matrix(aa,length(aa),1)
+  return(rev(prod[,1]))
 }
 
