82496b40789390116875da7c3008f62247b44107
tuckermcelroy <neidev1@yahoo.com>
2018-10-29 12:25:27 -0400

    deprecation
    files no longer needed in sigex

--- R/polymul.r
+++
@@ -1,6 +0,0 @@
-polymul <- function(a,b)
-{
-	u <- convolve(Re(a),rev(Re(b)),type="open") - convolve(Im(a),rev(Im(b)),type="open")
-	v <- convolve(Im(a),rev(Re(b)),type="open") + convolve(Re(a),rev(Im(b)),type="open")
-	return(Re(u + 1i*v))
-}
