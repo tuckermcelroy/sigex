959c30c0c1e76ae77b83b4803500870a85e62b98
jlivsey <james.a.livsey@gmail.com>
2022-03-01 13:06:58 -0500

    add all maxinit = 2500 model dirs

--- tests/BFS/BFS_analysis/source-loop.R
+++ tests/BFS/BFS_analysis/source-loop.R
@@ -4,19 +4,42 @@ topDir <- 'tests/BFS/BFS_analysis/'
 
 # Directories containing launch.R files to be run
 sourceDirs <- c(
-  "JD_A2_model",
-  "JD_A3_model",
-  "JD_A4_model",
-  "JD_A5_model",
-  "JD_A6_model",
-  "US_A2_model",
-  "US_A3_model",
-  "US_A4_model",
-  "US_A5_model",
-  "US_A6_model"
+  "JD_ser1_2101_nreg00_maxit2500_initmdl_A2",
+  "JD_ser1_2201_nreg00_maxit2500_initmdl_A3",
+  "JD_ser1_3201_nreg00_maxit2500_initmdl_A4",
+  "JD_ser1_2210_nreg00_maxit2500_initmdl_A5",
+  "JD_ser1_4001_nreg00_maxit2500_initmdl_A6",
+  "US_ser1_2101_nreg00_maxit2500_initmdl_A2",
+  "US_ser1_2201_nreg00_maxit2500_initmdl_A3",
+  "US_ser1_3201_nreg00_maxit2500_initmdl_A4",
+  "US_ser1_2210_nreg00_maxit2500_initmdl_A5",
+  "US_ser1_4001_nreg00_maxit2500_initmdl_A6"
 )
 
-# Main loop sourcing all launch.R files in specified directories
+# Check that all files have runMLE flag = TRUE
+countFlag <- 0
+for(d in sourceDirs){
+    if(sum(grepl("runMLE <- TRUE", readLines(launchFile))) > 0){
+      countFlag <- countFlag + 1
+    }
+}
+length(sourceDirs)
+countFlag
+
+# !!!! CAREFUL: Delete results.RData from
+if(FALSE){
+    for(d in sourceDirs){
+      launchPath <- paste0(topDir, d)
+      launchFile <- file.path(launchPath, 'launch.R')
+      print("Deleting results.RData from:")
+      print(d)
+      resultsFile <- file.path(launchPath, "results.RData")
+      if(file.exists(resultsFile)) file.remove(resultsFile)
+    }
+}
+
+
+# ---- Main loop sourcing all launch.R files in specified directories ----
 for(d in sourceDirs){
   launchPath <- paste0(topDir, d)
   launchFile <- file.path(launchPath, 'launch.R')
