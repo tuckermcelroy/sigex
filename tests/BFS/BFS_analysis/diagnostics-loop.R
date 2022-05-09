6ba1a566ee1cf3dfb552e3e3ecc7dd39904a00d4
jlivsey <james.a.livsey@gmail.com>
2022-05-09 09:19:40 -0400

    upd BFS diagnostics scripts

--- tests/BFS/BFS_analysis/diagnostics-loop.R
+++ tests/BFS/BFS_analysis/diagnostics-loop.R
@@ -4,37 +4,37 @@ topDir <- 'tests/BFS/BFS_analysis/'
 # List off all directories in top-level directory
 list.dirs(topDir, full.names = "FALSE")
 
+grep("JD_ser1_2101_nreg00_maxit[0-9]{4}",
+     x = list.dirs(topDir, full.names = "FALSE"),
+     value = TRUE)
+
+
 # List of models (directories) to load results.RData for
 model_directories <-
   c(
-    "JD_ser1_2101_nreg00_maxit2500_initmdl_A2",
-    "JD_ser1_2101_nreg00_maxit2500_initmdl_A2",
-    "JD_ser1_2201_nreg00_maxit2500_initmdl_A3",
-    "JD_ser1_3201_nreg00_maxit2500_initmdl_A4",
-    "JD_ser1_2210_nreg00_maxit2500_initmdl_A5",
-    "JD_ser1_4001_nreg00_maxit2500_initmdl_A6",
-    "US_ser1_2101_nreg00_maxit2500_initmdl_A2",
-    "US_ser1_2201_nreg00_maxit2500_initmdl_A3",
-    "US_ser1_3201_nreg00_maxit2500_initmdl_A4",
-    "US_ser1_2210_nreg00_maxit2500_initmdl_A5",
-    "US_ser1_4001_nreg00_maxit2500_initmdl_A6"
+    "JD_ser2_2101_nreg00_maxit0050_initmdl_A",
+    "JD_ser2_2101_nreg12_maxit0050_allReg_B",
+    "JD_ser2_2101_nreg03_maxit0050_signifReg_C",
+    "JD_ser2_0101_nreg00_maxit0050_visual_D"
   )
 
-par(mfrow = c(2, 3), mar = c(5, 4, 4, 2))
+par(mfrow = c(2, 2), mar = c(5, 4, 4, 2))
 for(d in model_directories){
   # Load results file and create short model name
   load(file.path(topDir, d, 'results.RData'))
   print(d)
 
   # Check convergence of optim
-  optimConverge <- fit.mle[[1]]$convergence == 0
-  print(optimConverge)
-  print(fit.mle[[1]]$convergence)
+  print(paste("Optim convergence code = ", fit.mle[[1]]$convergence))
+  # optimConverge <- fit.mle[[1]]$convergence == 0
 
   # Seasonal Theta (or seasonal AR)
   PAR <- sigex.psi2par(psi = fit.mle[[1]]$par, mdl = mdl, data.ts = data.ts)
   seasTheta <- PAR[[3]][[1]][sum(order[1:2]) + 1]
-  seasTheta <- round(seasTheta, 3)
+  # seasTheta <- round(seasTheta, 3)
+  message("Seasonal theta = ", seasTheta)
+  print(PAR)
+  print("-------------------------------------------")
 
   # Make acf plot of residuals
   if(TRUE){
@@ -50,3 +50,11 @@ for(d in model_directories){
     resid.acf <- acf(resid.mle, lag.max = 4 * 53, plot = TRUE, main = plotTitle)
   }
 }
+
+
+
+
+
+
+
+
--- tests/BFS/BFS_analysis/diagnostics.R
+++ tests/BFS/BFS_analysis/diagnostics.R
@@ -31,3 +31,62 @@ sigex.tstats(mdl,psi.mle,hess,constraint)
 ## model checking
 sigex.portmanteau(resid.mle, 4*period, length(psi.mle))
 sigex.gausscheck(resid.mle)
+
+
+# ---- Signal extraction from fit mdl ----
+# bundle
+analysis.mle <- sigex.bundle(data.ts, transform, mdl, psi.mle)
+
+## load up the fitted model for signal extraction
+dataNA.ts <- analysis.mle[[1]]
+mdl <- analysis.mle[[3]]
+psi <- analysis.mle[[4]]
+param <- sigex.psi2par(psi, mdl, dataNA.ts)
+param2 <- with(analysis.mle, sigex.psi2par(psi, model, data))
+
+## get fixed effects
+reg.trend <- sigex.fixed(data.ts,mdl,1,param,"Trend")
+# reg.nyd <- sigex.fixed(data.ts,mdl,1,param,"NewYearDay")
+# reg.mlk <- sigex.fixed(data.ts,mdl,1,param,"MLK")
+dataLIN.ts <- data.ts - ts(reg.trend,
+                           start=start(data.ts),frequency=period)
+
+## define trend and SA weekly filters
+week.period <- 365.25/7
+half.len <- floor(week.period/2)
+x11.filters <- x11filters(week.period,1)
+trend.filter <- x11.filters[[1]]
+seas.filter <- x11.filters[[2]]
+sa.filter <- x11.filters[[3]]
+shift <- (dim(sa.filter)[3]-1)/2
+
+## compute extractions
+trend.comp <- sigex.adhocextract(psi,mdl,dataNA.ts,trend.filter,half.len,0,TRUE)
+sa.comp <- sigex.adhocextract(psi,mdl,dataNA.ts,sa.filter,shift,0,TRUE)
+# AO.errs <- dataLIN.ts[AO.times] - data.casts[[1]]
+# sa.comp[[1]][AO.times] <- sa.comp[[1]][AO.times] + AO.errs
+
+## plotting
+trendcol <- "tomato"
+cyccol <- "orchid"
+seascol <- "seagreen"
+sacol <- "navyblue"
+fade <- 60
+
+#pdf(file="bfs-signal-ba.pdf",height=8,width=10)
+plot(data.ts)
+sigex.graph(extract = trend.comp,
+            reg = reg.trend,
+            start.date = start(data.ts),
+            period = period,
+            series = 1,
+            displace = 0,
+            color = trendcol,
+            fade = fade)
+sigex.graph(sa.comp,reg.trend,start(data.ts),
+            period,1,0,sacol,fade)
+# dev.off()
+
+## spectral diagnostics: seasonal adjustment
+sigex.specar(sa.comp[[1]],FALSE,1,period)
+dev.off()
