648989d488cb6ba5f3bc8bc67d07b8a718f35202
J. Livsey <james.a.livsey@gmail.com>
2023-11-15 12:27:09 -0500

    upds after running usethis::use_rcpp()

---
+++ R/sigex-package.R
@@ -0,0 +1,5 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib sigex, .registration = TRUE
+## usethis namespace: end
+NULL
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // polymul_matt
 arma::cube polymul_matt(arma::cube amat, arma::cube bmat);
 RcppExport SEXP _sigex_polymul_matt(SEXP amatSEXP, SEXP bmatSEXP) {
